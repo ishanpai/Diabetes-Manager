5f8f4edca811bc57041c5bf76558b59bb172491b
Ishan Paidhungat <ishanpaidhungat@gmail.com>
2025-11-06 16:18:22 +0530

    little fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,23 +1,21 @@
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string
-      email: string
-      name?: string | null
-      image?: string | null
-    }
+import type { DefaultSession, DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: DefaultSession['user'] & {
+      id: string;
+      email: string;
+    };
   }
 
-  interface User {
-    id: string
-    email: string
-    name?: string | null
-    image?: string | null
+  interface User extends DefaultUser {
+    id: string;
+    email: string;
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   interface JWT {
-    id: string
+    id: string;
   }
-} 
+}
